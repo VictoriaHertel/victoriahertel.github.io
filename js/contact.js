57565f48bdd8aa0669f007d04ac3454f7c250622
vhertel <v.hertel@gmx.de>
2024-02-15 05:36:24 +0800

    email

--- js/contact.js
+++ js/contact.js
@@ -1,12 +1,12 @@
 // CONTACT PAGE BEGINNING
 
 window.onload = function() {
-    var user = 'victoria.hertel.velasco';
-    var domain = 'gmail.com';
+    var user = 'v.hertel';
+    var domain = 'gmx.de';
     var element = document.createElement('a');
     
     element.href = 'mailto:' + user + '@' + domain;
-    element.innerHTML = 'victoria.hertel.velasco@gmail.com';
+    element.innerHTML = 'v.hertel@gmx.de';
     element.className = 'email-link';
 
     document.getElementById('email').appendChild(element);
